5ebb4a29ff6becbbb4e811cd19d987623fb35ae8
Luke Egan <eganlu@tcd.ie>
2016-02-20 00:02:33 +0000

    fixed a the login bugs and formatted the forms to fit bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  authenticated :user do
+    root :to => 'articles#index', as: :authenticated_root
+  end
+  root :to => 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
